7a8528cd2e5bcbc5c9f77630574e3afa731aa379
Javier Figueroa <javier.o.figueroa@gmail.com>
2018-06-12 15:18:40 -0400

    Updated Movement

--- Fluid puzzle prototype/Assets/Scripts/MovimientoBarco.cs
+++ Fluid puzzle prototype/Assets/Scripts/MovimientoBarco.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class MovimientoBarco : MonoBehaviour {
 
+	public float boatSpeed;
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.tag == "Goal"){ Debug.Log("Goal"); }
+
+		if(collision.tag == "Obstacle"){ 
+			Debug.Log("Obstacle"); 
+			boatSpeed = 0.0f;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(); 		
+		transform.Translate(0*boatSpeed*Time.deltaTime, 
+							0*boatSpeed*Time.deltaTime, 
+							1.0f*boatSpeed*Time.deltaTime);
 	}
 }
