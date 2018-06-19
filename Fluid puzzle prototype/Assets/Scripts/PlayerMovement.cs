9625b3ddbb29a9bff9c0b37a0d68bb47b3ffebe4
Javier Figueroa <javier.o.figueroa@gmail.com>
2018-06-18 20:38:40 -0400

    updated code base

---
+++ Fluid puzzle prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float playerSpeed;
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.tag == "Goal"){ Debug.Log("Goal"); }
+
+		if(collision.tag == "Obstacle"){ playerSpeed = 0.0f; }
+	}
+
+	public void move(Vector2 currentVec, float angle){
+
+		this.transform.Rotate(0.0f * Time.deltaTime, 
+							 (angle - Mathf.Asin(this.transform.localRotation.y)*360/Mathf.PI) * Time.deltaTime , 
+							 0.0f * Time.deltaTime);
+
+		this.transform.Translate(currentVec.x * Time.deltaTime * playerSpeed, 
+								 0.0f * Time.deltaTime * playerSpeed, 
+								 currentVec.y * Time.deltaTime * playerSpeed,
+								 Space.World);
+	}
+}
--- Fluid puzzle prototype/Assets/Scripts/VectorField.cs
+++ Fluid puzzle prototype/Assets/Scripts/VectorField.cs
@@ -11,32 +11,30 @@ public class VectorField : MonoBehaviour {
 
 	Vector2[,] vecField;
 	Vector2 currentVec;
-	float angle, alpha;
+	float angle, alpha, playerSpeed;
 
 	// Use this for initialization
 	void Start () {
+	
 		int vecFieldX = Mathf.CeilToInt(field.localScale.x * vecFieldRes); 
 		int vecFieldY = Mathf.CeilToInt(field.localScale.z * vecFieldRes);
 
+		// Initialize Vector Field
 		vecField = new Vector2[vecFieldX , vecFieldY];
-
 		for(int i = 0; i < vecFieldX; i++){
 			for(int j = 0; j < vecFieldY; j++){
-				vecField[i,j] = vectorField(i, j);
+				vecField[i,j] = vectorField(1, 1);
 			}
 		}
 
+		// Instantiate Vector Field arrows
 		Quaternion q = new Quaternion();
 		for (int i = 0; i < field.localScale.x; i++) {
 			for (int j = 0; j < field.localScale.z; j++) {
 				currentVec = vecField[Mathf.CeilToInt(field.localScale.x * vecFieldRes)-1, Mathf.CeilToInt(field.localScale.y * vecFieldRes)-1];
-				if(currentVec.x > 0){
-					alpha = Mathf.Atan(currentVec.y/currentVec.x)*180/Mathf.PI;
-					angle = 90 - alpha;
-				}else{
-					alpha = Mathf.Atan(currentVec.y/-currentVec.x)*180/Mathf.PI;
-					angle = alpha - 90;
-				}	
+
+				angle = calculateAngle(currentVec);
+
 				q.eulerAngles = new Vector3(0.0f, angle, 0.0f); 
 				Instantiate(vector, new Vector3(i + 0.5f, 0.5f, j + 0.5f), q);
 			}
@@ -46,28 +44,26 @@ public class VectorField : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// Move player if inside field
 		if(player.transform.position.x < field.localScale.x && player.transform.position.z < field.localScale.z &&
 		   player.transform.position.x > 0 && player.transform.position.z > 0){
 
 			currentVec = vecField[Mathf.FloorToInt(player.transform.position.x * vecFieldRes) , 
 			  					  Mathf.FloorToInt(player.transform.position.z * vecFieldRes)];
 
-			if(currentVec.x > 0){
-				alpha = Mathf.Atan(currentVec.y/currentVec.x)*180/Mathf.PI;
-			}else{
-				alpha = Mathf.Atan(currentVec.y/-currentVec.x)*180/Mathf.PI;
-			}	
+			angle = calculateAngle(currentVec);
 
-			player.transform.Rotate(0.0f * Time.deltaTime, 
-									(angle - Mathf.Asin(player.transform.localRotation.y)*360/Mathf.PI) * Time.deltaTime , 
-									0.0f * Time.deltaTime);
-
-			player.transform.Translate(currentVec.x * Time.deltaTime, 
-									   0.0f * Time.deltaTime, 
-							 		   currentVec.y * Time.deltaTime,
-									   Space.World);
+			player.GetComponent<PlayerMovement>().move(currentVec, angle);
 		}
 	}
 
-	Vector2 vectorField(int x, int y){ return new Vector2(x/100, y/100); }
+	float calculateAngle(Vector2 currentVec){
+		if(currentVec.x > 0){
+			return 90 - Mathf.Atan(currentVec.y/currentVec.x)*180/Mathf.PI;
+		}else{
+			return Mathf.Atan(currentVec.y/-currentVec.x)*180/Mathf.PI - 90;
+		}	
+	}
+
+	Vector2 vectorField(int x, int y){ return new Vector2(x, y); }
 }
